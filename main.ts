889a0eda14e408c5648939bcf5e83468616e38da
Jeremy Kings <47997513+jkingsDigiPen@users.noreply.github.com>
2021-07-12 10:46:58 -0700

    Create melodies in on start, use in forever loops.

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+function create_music () {
+    melody1 = "A A E G A A E G "
+    melody2 = "A E C5 E B E G E "
+    melody3 = "C5 - E - D - B - "
+    music.setVolume(50)
+}
 function create_player_cat () {
     spriteCat = sprites.create(assets.image`cat_frame_0`, SpriteKind.Player)
     controller.moveSprite(spriteCat)
@@ -8,15 +14,18 @@ function create_map () {
     tiles.setTilemap(tilemap`map_01`)
 }
 let spriteCat: Sprite = null
+let melody3 = ""
+let melody2 = ""
+let melody1 = ""
 create_map()
 create_player_cat()
-music.setVolume(50)
+create_music()
 forever(function () {
-    music.playMelody("C5 - E - D - B - ", 360)
+    music.playMelody(melody1, 90)
 })
 forever(function () {
-    music.playMelody("A A A A E E G G ", 180)
+    music.playMelody(melody3, 360)
 })
 forever(function () {
-    music.playMelody("A E C5 E B E G E ", 360)
+    music.playMelody(melody2, 360)
 })
